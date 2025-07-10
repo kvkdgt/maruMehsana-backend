6a2d5dab45f071d32c9f3101ba62a2cfcb408762
kvkdgt <kvkdgt12345@gmail.com>
2025-07-10 22:55:11 +0530

    new changes

--- resources/views/agency/layout/app.blade.php
+++ resources/views/agency/layout/app.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <title>@yield('title', 'Agency Dashboard') - NewsHub</title>
+    <title>@yield('title', 'Agency Dashboard') - MaruMehsana</title>
     
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800&display=swap" rel="stylesheet">
     <script src="https://cdnjs.cloudflare.com/ajax/libs/alpinejs/3.10.2/cdn.js" defer></script>
@@ -299,7 +299,7 @@
                             <path d="M10 6h8v4h-8z"/>
                         </svg>
                     </div>
-                    <span class="brand-text">NewsHub</span>
+                    <span class="brand-text">MaruMehsana</span>
                 </a>
             </div>
 
